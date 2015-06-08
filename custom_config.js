44aa5e8bf6514db6ba3291b84a6dfabbd501a56e
Manish <manish@Manishs-Mac-Pro.local>
2015-06-08 21:36:42 +0530

    first commit

---
+++ custom_config.js
@@ -0,0 +1,12 @@
+/*
+ * This is the iWebCab Customer App Custom Config File
+ * Created by James Parmee Morris (c) 2014-2015 all rights reserved
+ * Created for iWebCab.com service usage only.
+ */
+var api_key = "65c9abb0b63ce730de0346147c4513a7";
+var brand_name = "Ambassador Transportation Services";
+var brand_location = "Arkansas, USA";
+var dispatch_number = "+14792501594";
+
+// Specific Firm Requirements
+var allow_instant_jobs = true;
